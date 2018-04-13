bbb4a81140e94b107292e4182ba9e42bf3b064be
Bin Liu <bin.liu@magicwindow.cn>
2018-04-13 10:49:56 +0800

    Update Lua 函数

--- heidsoft.lua
+++ heidsoft.lua
@@ -31,3 +31,33 @@ if c then
 else
     print("not c")    -->output:not c
 end
+
+
+--[[ 函数返回两个值的最大值 --]]
+function max(num1, num2)
+
+   if (num1 > num2) then
+      result = num1;
+   else
+      result = num2;
+   end
+
+   return result; 
+end
+-- 调用函数
+print("两值比较最大值为 ",max(10,4))
+print("两值比较最大值为 ",max(5,6))
+
+
+myprint = function(param)
+   print("这是打印函数 -   ##",param,"##")
+end
+
+function add(num1,num2,functionPrint)
+   result = num1 + num2
+   -- 调用传递的函数参数
+   functionPrint(result)
+end
+myprint(10)
+-- myprint 函数作为参数传递
+add(2,5,myprint)
